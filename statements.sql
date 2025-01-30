746b7f2015a3c59cfa40086f1ddae4af2c63dc5a
RiccardoS <rspinellir13@gmail.com>
2025-01-30 17:01:24 +0100

    Ejercicio por terminar

--- statements.sql
+++ statements.sql
@@ -2,14 +2,26 @@
 -- PASO 1
 -- Tu código aquí
 
+CREATE TABLE usuarios (
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    edad INT
+);
 
 -- PASO 2
 -- Tu código aquí
 
+CREATE TABLE roles (
+    id_rol INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_rol VARCHAR(50) NOT NULL
+);
 
 -- PASO 3
 -- Tu código aquí
 
+ALTER TABLE usuarios ADD COLUMN id_rol INT;
 
 -- PASO 4
 -- Tu código aquí
